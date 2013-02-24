901bf2ade038ae12e7a43370a218298edf4fe6a8
TheWhisp <daavis.90@gmail.com>
2013-02-24 16:53:57 +0200

    quick implementation for 4.2.x

--- prebuilt/etc/init/init.qcom.bt.sh
+++ prebuilt/etc/init/init.qcom.bt.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
+# Copyright (c) 2009, Code Aurora Forum. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -30,8 +30,6 @@ BLUETOOTH_SLEEP_PATH=/proc/bluetooth/sleep/proto
 LOG_TAG="qcom-bluetooth"
 LOG_NAME="${0}:"
 
-hciattach_pid=""
-
 loge ()
 {
   /system/bin/log -t $LOG_TAG -p e "$LOG_NAME $@"
@@ -45,86 +43,28 @@ logi ()
 failed ()
 {
   loge "$1: exit code $2"
+  setprop bluetooth.status off;
   exit $2
 }
 
-start_hciattach ()
-{
-  /system/bin/hciattach -n $BTS_DEVICE $BTS_TYPE $BTS_BAUD &
-  hciattach_pid=$!
-  logi "start_hciattach: pid = $hciattach_pid"
-  echo 1 > $BLUETOOTH_SLEEP_PATH
-}
+setprop bluetooth.status off
 
-kill_hciattach ()
-{
-  echo 0 > $BLUETOOTH_SLEEP_PATH
-  logi "kill_hciattach: pid = $hciattach_pid"
-  ## careful not to kill zero or null!
-  kill -TERM $hciattach_pid
-  # this shell doesn't exit now -- wait returns for normal exit
-}
+QSOC_DEVICE=${1:-"/dev/ttyHS0"}
+QSOC_BAUD=${3:-"3000000"}
 
-# mimic hciattach options parsing -- maybe a waste of effort
-USAGE="hciattach [-n] [-p] [-b] [-t timeout] [-s initial_speed] <tty> <type | id> [speed] [flow|noflow] [bdaddr]"
+# echo 1 > $BLUETOOTH_SLEEP_PATH
 
-while getopts "blnpt:s:" f
-do
-  case $f in
-  b | l | n | p)  opt_flags="$opt_flags -$f" ;;
-  t)      timeout=$OPTARG;;
-  s)      initial_speed=$OPTARG;;
-  \?)     echo $USAGE; exit 1;;
-  esac
-done
-shift $(($OPTIND-1))
+/system/bin/hci_qcomm_init -d $QSOC_DEVICE -s $QSOC_BAUD 
 
-# Note that "hci_qcomm_init -e" prints expressions to set the shell variables
-# BTS_DEVICE, BTS_TYPE, BTS_BAUD, and BTS_ADDRESS.
-
-#Selectively Disable sleep
-BOARD=`getprop ro.product.device`
-
-POWER_CLASS=`getprop qcom.bt.dev_power_class`
-
-#find the transport type
-TRANSPORT=`getprop ro.qualcomm.bt.hci_transport`
-logi "Transport : $TRANSPORT"
-
-case $POWER_CLASS in
-  1) PWR_CLASS="-p 0" ;
-     logi "Power Class: 1";;
-  2) PWR_CLASS="-p 1" ;
-     logi "Power Class: 2";;
-  3) PWR_CLASS="-p 2" ;
-     logi "Power Class: CUSTOM";;
-  *) PWR_CLASS="";
-     logi "Power Class: Ignored. Default(1) used (1-CLASS1/2-CLASS2/3-CUSTOM)";
-     logi "Power Class: To override, Before turning BT ON; setprop qcom.bt.dev_power_class <1 or 2 or 3>";;
-esac
-
-eval $(/system/bin/hci_qcomm_init -e $PWR_CLASS && echo "exit_code_hci_qcomm_init=0" || echo "exit_code_hci_qcomm_init=1")
+exit_code_hci_qcomm_init=$?
 
 case $exit_code_hci_qcomm_init in
-  0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE $BTS_TYPE $BTS_BAUD";;
+  0) logi "Bluetooth QSoC firmware download succeeded";;
   *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_init;;
 esac
 
-# init does SIGTERM on ctl.stop for service
-trap "kill_hciattach" TERM INT
+setprop bluetooth.status on
 
-case $TRANSPORT in
-    "smd")
-        logi "Seting property to insert the hci smd transport module"
-        setprop bt.hci_smd.driver.load 1
-     ;;
-     *)
-        logi "start hciattach"
-        start_hciattach
-
-        wait $hciattach_pid
-        logi "Bluetooth stopped"
-     ;;
-esac
+logi "start bluetooth transport"
 
 exit 0
