29d33ed695ce6de8780567d1248088023b2e892d
TheWhisp <daavis.90@gmail.com>
2013-03-16 16:18:28 +0200

    Fix bluetooth
    Gingerbread uses older Bluez version than ICS/JB
    Forward-port some changes from Samsung's opensource package

--- patches/install.sh
+++ patches/install.sh
@@ -5,3 +5,7 @@ cd frameworks/base
 echo "Applying frameworks/base patches..."
 git am $rootdirectory/device/samsung/jena/patches/frameworks_base/*.patch
 cd $rootdirectory
+cd external/bluetooth/bluez
+echo "Applying external/bluetooth/bluez patches..."
+git am $rootdirectory/device/samsung/jena/patches/external_bluetooth_bluez/*patch
+cd $rootdirectory
